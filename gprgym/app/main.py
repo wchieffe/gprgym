1bf41b9f113e918b5c8ec343aec72f77d920d2b1
Walker Chieffe <wchieffe@gmail.com>
2023-07-06 17:21:13 -0600

    initial pass at move_to_position

--- gprgym/app/main.py
+++ gprgym/app/main.py
@@ -1,23 +1,37 @@
-from fastapi import FastAPI
-import uvicorn
+#launch Isaac Sim before any other imports
+#default first two lines in any standalone application
+from omni.isaac.kit import SimulationApp
+simulation_app = SimulationApp({"headless": False}) # we can also run as headless.
 
-from helpers import is_simulation_running, launch_simulation, stop_simulation_process
-    
-app = FastAPI()
+import signal
+import time
+import zmq
 
-@app.get("/start-simulation")
-def start_simulation():
-    if is_simulation_running():
-        return {"message": "Simulation is already running."}
-    pid = launch_simulation()
-    return {"message": "Simulation started.", "pid": pid}
-
-@app.get("/stop-simulation")
-def stop_simulation():
-    if not is_simulation_running():
-        return {"message": "Simulation is not running."}
-    stop_simulation_process()
-    return {"message": "Simulation stopped."}
+from helpers import setup_scene, pick_place, move_to_position
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    world, franka, fancy_cube = setup_scene()
+
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    socket.bind("tcp://*:5558")
+
+    while True:
+        print("Waiting for event")
+        message = socket.recv()
+        print(f"Received request: {message}")
+
+        if message == b"pick":
+            pick_place(world, franka, fancy_cube)
+        elif message == b"move to":
+            move_to_position(world, franka)
+        else:
+            break
+
+        response = b"Success!"
+        socket.send(response)
+
+        time.sleep(1)
+
+    simulation_app.close() # close Isaac Sim
